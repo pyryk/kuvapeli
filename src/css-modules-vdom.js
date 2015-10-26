64ba8b53e5ae573281e595cac675cf32cbb4b700
Pyry Kröger <pyry.kroger@futurice.com>
2015-10-26 10:32:21 +0200

    add support for ignoring certain classes when generating css-modules classNames

--- src/css-modules-vdom.js
+++ src/css-modules-vdom.js
@@ -1,8 +1,17 @@
 import {h} from '@cycle/dom';
 
-export default (styles) => function h2(selector, ...args) {
+function classIsReplaced(ignoredClasses, className) {
+	return ignoredClasses.indexOf(className) === -1;
+}
+
+function getClassName(className, styles, ignoredClasses) {
+	return classIsReplaced(ignoredClasses, className) ? ('.' + styles[className]) : ('.' + className);
+}
+
+
+export default (styles, ...ignoredClasses) => function h2(selector, ...args) {
 	let [element, ...ids] = selector.split('.');
 	
 
-	return h.apply(h, [element + ids.map(id => '.' + styles[id])].concat(args));
+	return h.apply(h, [element + ids.map(id => getClassName(id, styles, ignoredClasses))].concat(args));
 };
--- src/tabs.js
+++ src/tabs.js
@@ -3,9 +3,9 @@ import {h} from '@cycle/dom';
 import vc from './css-modules-vdom';
 import styles from './app.css';
 
-const h2 = vc(styles);
+const h2 = vc(styles, 'tab1', 'tab2');
 
-export function tabs({DOM, props$}, name = 'default') {
+export function tabs({DOM, props$}) {
   let initialValue$ = props$.map(props => props.default).first();
 
   // TODO make tab-number independent
@@ -16,20 +16,14 @@ export function tabs({DOM, props$}, name = 'default') {
 
   let vtree$ = Rx.Observable
     .combineLatest(props$, value$, (props, value) =>
-      h(`div.labeled-slider.${name}`, [
+      h(`div.tabs-container`, [
         h2('div.tabs', [
-          h2(`div.tab1-container${value === 'tab1' ? '.active' : ''}`, [
-            h(`div.tab1`,
-              ['Setup']
-            )
-          ]),
-          h2(`div.tab1-container${value === 'tab2' ? '.active' : ''}`, [
-            h(`div.tab2`,
-              ['Peli']
-            )
-          ])
-          
-          
+          h2(`div.tab1${value === 'tab1' ? '.active' : ''}`,
+            ['Setup']
+          ),
+          h2(`div.tab2${value === 'tab2' ? '.active' : ''}`,
+            ['Peli']
+          )
         ]),
         h('div.container', value === 'tab1' ? props.children[0] : props.children[1])
       ])
