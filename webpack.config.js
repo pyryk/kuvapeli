74705b574075b584db0b22fc09a1d370dcea5d0c
Pyry Kröger <pyry.kroger@futurice.com>
2015-11-06 19:47:50 +0200

    add sample images for easier testing

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "bin"),
         publicPath: "/bin/",
+        path: "public",
         filename: "bundle.js"
     },
     module: {
